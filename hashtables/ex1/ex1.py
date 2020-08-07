c72546661119a000c09baeac7042d59ce0341809
karlmanalo <karlmanalo@comcast.net>
2020-08-07 11:25:34 -0600

    Revert "Turns in CS U2S1 SC"
    This reverts commit 8ce4141c303c328952ecc446d2085ac4d34ebde0.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,32 +1,7 @@
 def get_indices_of_item_weights(weights, length, limit):
-    # Instantiate an empty cache and result list
-    cache = {}
-    result = []
-
     """
-    Create a cache where the keys are the weights and the values are
-    the index values. Create these index values as lists to handle
-    duplicate keys.
+    YOUR CODE HERE
     """
-
-    for index, weight in enumerate(weights):
-        if weight not in cache:
-            cache[weight] = [index]
-        else:
-            cache[weight].append(index)
-
-    """
-    Iterate through each weight (weight_1) in our list. If limit - weight_1 
-    (weight_2) exists in our cache, we need to return the index values of
-    both weight_1 and weight_2. Return a tuple of indices, where the first
-    index is the "higher" index, numerically speaking. Because the values
-    in our cache were stored as lists (even if they are 1 element long),
-    we must call max/min on these lists to return an index, and again call
-    max/min on the resulting tuples to find our result.
-    """  
-
-    for index, weight in enumerate(weights):
-        if limit - weight in cache:
-            return (max(index, max(cache[limit - weight])), min(index, min(cache[limit - weight])))
+    # Your code here
 
     return None
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,42 +1,15 @@
+# Your code here
+
+
+
 def finder(files, queries):
-   
-    # Instantiate an empty cache and result list
-    cache = {}
-    result = []
-
-
     """
-    To create our cache, we must first figure out what the most
-    appropriate key would be. Since the queries are filenames, our goal
-    will be to create a cache where the keys are filenames and the
-    values are lists of complete filepaths with those filenames. To 
-    access the filenames from the filepaths, we utilize the .rsplit()
-    method, where we define the character / as our delimiter and only
-    split once (starting from the right). Now that we have our filename,
-    we can begin building our cache with this filename as our key. If
-    the filename does not yet exist in our cache, we define the value
-    in our cache to be a 1-element list containing the full filepath.
-    If the filename does exist in our cache, we append the filepath to
-    the existing list at that key in the cache.
+    YOUR CODE HERE
     """
-    for f in files:
-        key = f.rsplit("/", 1)[-1]
-        if key not in cache:
-            cache[key] = [f]
-        else:
-            cache[key].append(f)
-    
-    """
-    Now it's a simple matter of checking our cache for each of the 
-    queries. If the filename in queries exists in our cache, we extend
-    our result list with the list that exists at that filename's value
-    in the cache.
-    """
-    for q in queries:
-        if q in cache:
-            result.extend(cache[q])
+    # Your code here
 
     return result
+
 
 if __name__ == "__main__":
     files = [
